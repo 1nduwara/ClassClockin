48eb9193f3a48c52d92175bca2b5d9af6d404485
Induwara Dassanayake <ilbdassanayake123@outlook.com>
2024-07-21 16:14:46 +0530

    Commit Successful

--- app/src/main/java/com/example/classclockin/Fragments/HomeCards/ViewStudentList.kt
+++ app/src/main/java/com/example/classclockin/Fragments/HomeCards/ViewStudentList.kt
@@ -1,4 +1,4 @@
-package com.example.classclockin.Fragments
+package com.example.classclockin.Fragments.HomeCards
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -6,19 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.classclockin.R
-import com.example.classclockin.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
 
-    private lateinit var binding: FragmentHomeBinding
+class ViewStudentList : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return inflater.inflate(R.layout.fragment_view_student_list, container, false)
     }
+
+
 }
