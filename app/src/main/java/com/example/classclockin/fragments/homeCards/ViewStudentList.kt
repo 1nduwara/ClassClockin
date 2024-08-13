fff0b79cc78290417e9ec3f8b0bbd5f65fa7448d
Induwara Dassanayake <ilbdassanayake123@outlook.com>
2024-08-13 20:49:51 +0530

    Student List UI complete

--- app/src/main/java/com/example/classclockin/fragments/homeCards/ViewStudentList.kt
+++ app/src/main/java/com/example/classclockin/fragments/homeCards/ViewStudentList.kt
@@ -5,17 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.classclockin.R
+import com.example.classclockin.databinding.FragmentViewStudentListBinding
 
 
 class ViewStudentList : Fragment() {
+
+    private lateinit var binding: FragmentViewStudentListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_student_list, container, false)
+        binding = FragmentViewStudentListBinding.inflate(inflater, container, false)
+
+        binding.btnBack.setOnClickListener {
+            it.findNavController().navigate(R.id.action_viewStudentList_to_homeFragment)
+        }
+
+        return binding.root
     }
 
 
